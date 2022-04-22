072e77d6b8c32de1682319bedeb3687976980a16
Teja <starteja.007@gmail.com>
2022-04-22 12:49:11 +0530

    push to heroku

---
+++ app.js
@@ -0,0 +1,17 @@
+const { Console } = require("console");
+const express= require("express");
+const app=express();
+const port=process.env.PORT || 3000;
+
+app.use(express.static('.'))
+app.use(express.static("pubilc"));
+
+app.get("/",(req,res)=>{
+
+    res.sendFile(__dirname+'index.html')
+
+});
+
+app.listen(port,()=>{
+    console.log("Listening");
+})
